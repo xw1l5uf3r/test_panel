9ca946da642786d09928c2fed6e8cfed87e65e43
xw1l5uf3r <misev.vova@yandex.ru>
2026-03-14 05:10:14 +0300

    Update logStore.ts

--- app/logStore.ts
+++ app/logStore.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export type Log = {
   id: number
   type: "error" | "warning"
@@ -28,35 +27,4 @@ export function pushLog(type: Log["type"], text: string) {
     logs = logs.filter((l) => l.id !== log.id)
     listener?.(logs)
   }, 10000)
-=======
-export type Log = {
-  id: number
-  type: "error" | "warning"
-  text: string
-}
-
-let logs: Log[] = []
-
-let listener: ((logs: Log[]) => void) | null = null
-
-export function setLogListener(fn: (logs: Log[]) => void) {
-  listener = fn
-}
-
-export function pushLog(type: Log["type"], text: string) {
-  const log = {
-    id: Date.now(),
-    type,
-    text
-  }
-
-  logs = [...logs, log]
-
-  listener?.(logs)
-
-  setTimeout(() => {
-    logs = logs.filter((l) => l.id !== log.id)
-    listener?.(logs)
-  }, 10000)
->>>>>>> 10668668e591bbb09f4c012e6a2b97598338e54a
 }
