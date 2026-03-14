24dc88be4f00e30a7f45fc36069692039b005097
xw1l5uf3r <misev.vova@yandex.ru>
2026-03-14 05:09:47 +0300

    Update container.tsx

--- app/container.tsx
+++ app/container.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // components/ToastContainer.tsx
 "use client";
 import { useEffect, useState } from "react";
@@ -45,52 +44,4 @@ export default function ToastContainer() {
       ))}
     </div>
   );
-=======
-// components/ToastContainer.tsx
-"use client";
-import { useEffect, useState } from "react";
-import { setLogListener, Log } from "./logStore";
-import { Lexend } from 'next/font/google'
-
-const lexend = Lexend({ subsets: ['latin'], weight: ['400','500','600','700'] })
-export default function ToastContainer() {
-  const [logs, setLogs] = useState<Log[]>([]);
-
-  useEffect(() => {
-    setLogListener(setLogs);
-  }, []);
-
-  return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: 20,
-        right: 20,
-        display: "flex",
-        flexDirection: "column",
-        gap: 8,
-        zIndex: 9999,
-      }}
-    >
-      {logs.map((log) => (
-        <div
-
-          className={lexend.className}
-          key={log.id}
-          style={{
-            maxWidth: 200,
-            maxHeight: 100,
-            padding: "8px 12px",
-            overflow: "hidden",
-            borderRadius: 20,
-            color: "#fff",
-            background: log.type === "error" ? "#d9534f" : "#f0ad4e",
-          }}
-        >
-          {log.text}
-        </div>
-      ))}
-    </div>
-  );
->>>>>>> 10668668e591bbb09f4c012e6a2b97598338e54a
 }
